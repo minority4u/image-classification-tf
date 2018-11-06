f5fd28d4e0264664d23546b618b96a45a9509529
minority4u <emailsvenkoehler@gmx.net>
2018-11-06 12:22:00 +0100

    added training entry

---
+++ src/models/train_model_inceptionv3_1.py
@@ -0,0 +1,19 @@
+import logging
+from src.utils_io import Console_and_file_logger
+
+def train():
+    logging.info('training starts')
+
+
+
+
+
+
+
+def main():
+    train()
+
+
+if __name__ == '__main__':
+    Console_and_file_logger('Train_inception_v3')
+    main()
