e5b6b4fca5a19e776e289849ed88ee206f16930f
Maier <Dennis.Maier@accas-group.com>
2018-11-06 15:12:02 +0100

    Update src/data/make_dataset.py
    Added Generators

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -7,8 +7,6 @@ from dotenv import find_dotenv, load_dotenv
 import numpy as np
 from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
-from scipy.misc import imresize
-
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
@@ -20,55 +18,57 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
-def GetAllImages(dirPath):
+
+# Hardcode Config für Training und Validation
+def __GetImageDataGenerator__(validationSplit = 0.2):
     datagen = ImageDataGenerator(
         featurewise_center=False,
         featurewise_std_normalization=False,
-        preprocessing_function=preprocess)
+        rescale=(1. / 255),
+        shear_range=0,
+        zoom_range=0,
+        horizontal_flip=False,
+        vertical_flip=False,
+        #preprocessing_function=preprocess,
+        validation_split=validationSplit)
+    return datagen
 
+def GetTrainAndValidationGenerator(pathToData, imageSize = (224,224), batchSize =  32, classmode = 'categorical'):
+    imageDataGenerator = __GetImageDataGenerator__()
+    train_generator = __GetTrainGenerator__(imageDataGenerator, pathToData, imageSize, batchSize, classmode)
+    validation_generator = __GetValidationGenerator__(imageDataGenerator, pathToData, imageSize, batchSize, classmode)
+    return train_generator, validation_generator
+
+def __GetAllImages__(pathToData, imageSize = (224,224), batchSize =  32, classmode = 'categorical'):
+    datagen = __GetImageDataGenerator__()
     generator = datagen.flow_from_directory(
-        dirPath,
-        target_size=(48, 48),
-        batch_size=1024,
+        pathToData,
+        target_size=imageSize,
+        batch_size=batchSize,
         shuffle=True,
-        class_mode='sparse')
-
+        class_mode=classmode)
     inputs, targets = next(generator)
     return inputs, targets
 
-def GetTrainGenerator(dirPath):
-    train_datagen = ImageDataGenerator(
-        rescale=1. / 255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True,
-        preprocessing_function=preprocess)
-
-    train_generator = train_datagen.flow_from_directory(
-        dirPath,
-        target_size=(150, 150),
-        batch_size=32,
-        class_mode='binary')
-
+def __GetTrainGenerator__(imageDataGenerator,pathToData, imageSize = (224, 224), batchSize = 32, classmode = 'categorical'):
+    train_generator = imageDataGenerator.flow_from_directory(
+        pathToData,
+        target_size=imageSize,
+        batch_size=batchSize,
+        shuffle=True,
+        class_mode=classmode,
+        subset='training')
     return train_generator
 
-def GetValidationGenerator(dirPath):
-    test_datagen = ImageDataGenerator(rescale=1. / 255,
-                                      preprocessing_function=preprocess)
-    validation_generator = test_datagen.flow_from_directory(
-        dirPath,
-        target_size=(150, 150),
-        batch_size=32,
-        class_mode='binary')
-
+def __GetValidationGenerator__(imageDataGenerator, pathToData, imageSize = (224, 224), batchSize = 32, classmode = 'categorical'):
+    validation_generator = imageDataGenerator.flow_from_directory(
+        pathToData,
+        target_size=imageSize,
+        batch_size=batchSize,
+        shuffle=True,
+        class_mode=classmode,
+        subset='validation')
     return validation_generator
-
-    #model.fit_generator(
-    #   train_generator,
-    #    samples_per_epoch=2000,
-    #    nb_epoch=50,
-    #    validation_data=validation_generator,
-    #    nb_val_samples=800)
 
 
 if __name__ == '__main__':
@@ -83,23 +83,3 @@ if __name__ == '__main__':
     load_dotenv(find_dotenv())
 
     main()
-
-
-def preprocess(img):
-    width, height = img.shape[0], img.shape[1]
-    img = image.array_to_img(img, scale=False)
-
-    # Crop 48x48px
-    desired_width, desired_height = 48, 48
-
-    if width < desired_width:
-        desired_width = width
-    start_x = np.maximum(0, int((width-desired_width)/2))
-
-    img = img.crop((start_x, np.maximum(0, height-desired_height), start_x+desired_width, height))
-    img = img.resize((48, 48))
-
-    img = image.img_to_array(img)
-    return img / 255.
-
-
