5a86bc6c0ea6d3b37d69c1e3add4372fcd3f554f
DennisMaier <1621480@stud.hs-mannheim.de>
2018-11-21 13:03:24 +0100

    fix test

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -30,12 +30,10 @@ class_names = get_class_names()
 
 def evaluate_on_patch_level(evaluation_path):
     global model, graph
-    test_generator = __get_generator__(ImageDataGenerator(), evaluation_path,
-                                       (config['input_image_width'], config['input_image_height']),
-                                       config['batch_size_test'],
-                                       config['class_mode'],
-                                       'validation',
-                                       shuffle=False)
+
+    test_data_generator = ImageDataGenerator()
+    test_generator = test_data_generator.flow_from_directory(evaluation_path)
+
     logging.info("Eval path: " + evaluation_path)
     # evaluate on patch level
     predictions = model.predict_generator(test_generator, steps=None, max_queue_size=10, workers=1,
