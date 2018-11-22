d7dc27525ac177e08557882c4fa52e2f4b03dd22
DennisMaier <1621480@stud.hs-mannheim.de>
2018-11-22 11:25:25 +0100

    bugfix evaluate

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -71,7 +71,7 @@ def evaluate_on_image_level(evaluation_path):
     logging.debug('Classes found: {}'.format(len(inference_images)))
     # logging.debug('Images found: {}'.format(inference_images))
     global model, graph, class_names
-    results = [(label, external_predict_images(images, model, graph)) for label, images in inference_images]
+    results = [(label, external_predict_images(images, model, graph, conf=config)) for label, images in inference_images]
 
     #logging.info(results)
 
